8264c53b740de87744f0e7f92632fc26b9ad0932
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-23 15:45:19 -0300

    listagem usuarios

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,16 +1,68 @@
 <template>
-<div>
-    <div class="columns is-mobile is-centered">
-    <div class="column is-half"></div>
+  <div>
     <h1>Painel Adm</h1>
-  </div>
 
-</div>
-  
+    <table class="table">
+      <thead>
+        <tr>
+          <th><abbr title="Nome">Nome</abbr></th>
+          <th><abbr title="Email">E-mail</abbr></th>
+          <th><abbr title="Cargo">Cargo</abbr></th>
+          <th><abbr title="Cargo">Ações</abbr></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.role | processRole }}</td>
+          <td>
+            <button class="button is-small">Editar</button>|<button
+              class="button is-small"
+            >
+              Deletar
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  setup() {},
+  created() {
+    var header = {
+      headers: {
+        authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    };
+
+    axios
+      .get("http://ba1fa980b5ae.ngrok.io/user", header)
+      .then((response) => {
+        console.log(response);
+        this.users = response.data.users;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  data() {
+    return {
+      users: [],
+    };
+  },
+  filters: {
+    processRole: function (value) {
+      if (value == 0) {
+        return "Usuário comum";
+      } else if (value == 1) {
+        return "Admin";
+      }
+    },
+  },
 };
 </script>
 <style scoped>
