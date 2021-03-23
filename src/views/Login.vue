16163cf094c1ce4e80945345353b26c8495de14a
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-23 12:25:47 -0300

    componente login

---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <hr />
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <div class="notification is-info">
+          <div>
+            <div v-if="error != ''" class="notification is-danger">
+              <p>{{ error }}</p>
+            </div>
+            <hr />
+          </div>
+          <p>E-mail:</p>
+          <input
+            class="input"
+            type="email"
+            placeholder="email@email.com"
+            name=""
+            id=""
+            v-model="email"
+          />
+          <p>Senha:</p>
+          <input
+            class="input"
+            type="password"
+            placeholder="Senha"
+            name=""
+            id=""
+            v-model="password"
+          />
+          <hr />
+          <button @click="login" class="button is-success">Logar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//const axios = require("axios");
+
+export default {
+  data() {
+    return {
+      password: "",
+      email: "",
+      error: "",
+    };
+  },
+  methods: {
+    login: function () {
+      console.log("login");
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
