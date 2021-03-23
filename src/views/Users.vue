b9c58b1ff670c10ea0999f8910195ce68dea479d
benjamim <bneto@computacao.cua.ufmt.br>
2021-03-23 13:00:16 -0300

    painel administrativo

---
+++ src/views/Users.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <div class="columns is-mobile is-centered">
+    <div class="column is-half"></div>
+    <h1>Painel Adm</h1>
+  </div>
+
+</div>
+  
+</template>
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped>
+</style>
